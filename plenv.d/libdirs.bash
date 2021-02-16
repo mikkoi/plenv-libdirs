852516d00db00e440136c2e3164a2e6d0c35aa1e
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2021-02-16 23:49:59 +0100

    Import code
    Signed-off-by: Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>

---
+++ plenv.d/libdirs.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+PLENV_LIBDIRS="$(plenv-libdirs)"
+PLENV_PERL5LIB="${PERL5LIB-NOT_SET}"
+# If PERL5LIB is set (even if empty), we simply let it pass without changes
+if [ "$PLENV_PERL5LIB" = "NOT_SET" ]; then
+    if [ -n "$PLENV_LIBDIRS" ]; then
+        PERL5LIB="$PLENV_LIBDIRS"
+        export PERL5LIB
+    fi
+fi
