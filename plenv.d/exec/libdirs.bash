4b4c7c6e5614275149b2009a1b213229a80460d2
Mikko Koivunalho <mikkoi@cpan.org>
2023-09-27 08:58:24 +0300

    Rewrite plenv.d/exec/libdirs.bash
    Signed-off-by: Mikko Koivunalho <mikkoi@cpan.org>

--- plenv.d/exec/libdirs.bash
+++ plenv.d/exec/libdirs.bash
@@ -2,12 +2,7 @@
 set -o errexit -o nounset -o pipefail
 [ -n "${PLENV_DEBUG-}" ] && set -o xtrace
 
-PLENV_LIBDIRS="$(plenv-libdirs)"
-PLENV_PERL5LIB="${PERL5LIB-NOT_SET}"
-# If PERL5LIB is set (even if empty), we simply let it pass without changes
-if [ "$PLENV_PERL5LIB" = "NOT_SET" ]; then
-    if [ -n "$PLENV_LIBDIRS" ]; then
-        PERL5LIB="$PLENV_LIBDIRS"
-        export PERL5LIB
-    fi
+if ! declare -p 'PERL5LIB' &>/dev/null; then
+      PERL5LIB="$(plenv-libdirs)"
+      export PERL5LIB
 fi
